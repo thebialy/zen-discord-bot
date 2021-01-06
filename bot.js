2a02b8404566467e9e0e72cd7ad0ddebd06bd185
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-06 11:37:28 -0600

    bot can now reply to a certain message

--- bot.js
+++ bot.js
@@ -7,4 +7,10 @@ client.on('ready', () => {
   console.log('Bot is ready for war!')
 })
 
+client.on('message', msg => {
+    if (msg.content === "oscar") {
+        msg.reply("You're dead to me!")
+    }
+})
+
 client.login(process.env.BOT_TOKEN)
